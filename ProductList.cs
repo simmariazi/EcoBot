783a60a12fd53fadec0153db68bf54caadb0cc02
이레 <irene920609@gmail.com>
2021-08-02 00:06:52 +0900

    ProductList 클래스 추가

---
+++ ProductList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoBot
+{
+    class ProductList
+    {
+        public string Thumnail { get; set;}
+        public string ProductUrl { get; set; }
+        public int Id { get; set; }
+    }
+}
