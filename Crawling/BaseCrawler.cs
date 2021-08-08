9ce03e2fd143c677433ccbac4abd3dac37e39df6
김웅재 <hawoond@gmail.com>
2021-08-08 11:56:34 +0900

    상속 예제 추가(BaseCrawler, ProductListCrawler)

--- Crawling/BaseCrawler.cs
+++ Crawling/BaseCrawler.cs
@@ -8,5 +8,14 @@ namespace EcoBot.Crawling
 {
     public class BaseCrawler
     {
+        // 상속 예시를 위한 예제 함수 작성
+        public string InheritanceExample(string lalla)
+        {
+            string result = string.Empty;
+
+            result = lalla + lalla;
+
+            return result;
+        }
     }
 }
--- Crawling/ProductListCrawler.cs
+++ Crawling/ProductListCrawler.cs
@@ -8,5 +8,16 @@ namespace EcoBot.Crawling
 {
     public class ProductListCrawler : BaseCrawler
     {
+        public void memo()
+        {
+            // 상속 받은 BaseCrawler 클래스에서 InheritanceExample 함수 사용 예시
+            string sample = InheritanceExample("랄라");
+
+            // 의미 없는 for문 테스트
+            for (int i = 0; i < sample.Length; i++)
+            {
+                Console.WriteLine(sample);
+            }
+        }
     }
 }
